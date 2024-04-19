6c667f551062cd5861b533ee97d5835e83ba9731
CiliasC <163523733+CiliasC@users.noreply.github.com>
2024-04-19 12:06:24 +0100

    Experimentation

--- MyCheeseShop/Model/Cheese.cs
+++ MyCheeseShop/Model/Cheese.cs
@@ -8,7 +8,7 @@
         public string Description { get; set; }   
         public string Strength { get; set; }
         public decimal Price { get; set; }
-        public string ImageUrl { get; set; }
+        public string? ImageUrl { get; set; }
 
     }
 }
